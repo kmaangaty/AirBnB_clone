6433d9ddb554280733fd9f95c321a755ef3cce19
kmangaty <kmaangaty@gmail.com>
2024-02-14 19:10:05 +0200

    3

--- models/base_model.py
+++ models/base_model.py
@@ -5,9 +5,9 @@ from datetime import datetime
 
 
 class BaseModel:
-    """A base class for all hbnb models"""
+    """BaseModel"""
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """BaseModel"""
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
@@ -23,22 +23,22 @@ class BaseModel:
             self.__dict__.update(kwargs)
 
     def __str__(self):
-        """Returns a string representation of the instance"""
+        """BaseModel"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
+        """save"""
         from models import storage
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
+        """to_dict"""
+        dc = {}
+        dc.update(self.__dict__)
+        dc.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        dc['created_at'] = self.created_at.isoformat()
+        dc['updated_at'] = self.updated_at.isoformat()
+        return dc
